a204984fbaefaf6ee1dae7b01cd60e7f75c93d87
Smolarz <mjsz@gft.com>
2018-05-11 14:32:29 +0200

    Added Book and Author repositories

---
+++ spring5webapp/src/main/java/com/maciej/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package com.maciej.spring5webapp.repositories;
+
+import com.maciej.spring5webapp.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
