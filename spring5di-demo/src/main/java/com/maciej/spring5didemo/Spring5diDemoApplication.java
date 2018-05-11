1940aca3bea746452cf4876afe77e8a1b432c8a4
Smolarz <mjsz@gft.com>
2018-05-11 17:01:48 +0200

    SpringBoot basic structure to familiarize with DI

---
+++ spring5di-demo/src/main/java/com/maciej/spring5didemo/Spring5diDemoApplication.java
@@ -0,0 +1,12 @@
+package com.maciej.spring5didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5diDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5diDemoApplication.class, args);
+	}
+}
