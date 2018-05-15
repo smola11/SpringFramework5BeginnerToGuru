2d3b75b9fdc0d084643215e7ccf367979cb36513
Smolarz <mjsz@gft.com>
2018-05-15 14:16:35 +0200

    Spring Boot Joke App

---
+++ spring5jokesapp/src/main/java/com/maciej/spring5jokesapp/Spring5jokesappApplication.java
@@ -0,0 +1,12 @@
+package com.maciej.spring5jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5jokesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5jokesappApplication.class, args);
+	}
+}
---
+++ spring5jokesapp/src/main/java/com/maciej/spring5jokesapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.maciej.spring5jokesapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
