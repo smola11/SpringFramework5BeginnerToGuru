4b32f54b4f796fe62e00e8b0b2857cfcd2f4845f
Smolarz <mjsz@gft.com>
2018-05-15 17:20:20 +0200

    Property source added

--- spring5di-demo/src/main/java/com/maciej/spring5didemo/Spring5diDemoApplication.java
+++ spring5di-demo/src/main/java/com/maciej/spring5didemo/Spring5diDemoApplication.java
@@ -4,6 +4,7 @@ import com.maciej.spring5didemo.controllers.ConstructorInjectedController;
 import com.maciej.spring5didemo.controllers.MyController;
 import com.maciej.spring5didemo.controllers.PropertyInjectedController;
 import com.maciej.spring5didemo.controllers.SetterInjectedController;
+import com.maciej.spring5didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,9 +17,8 @@ public class Spring5diDemoApplication {
 
         MyController controller = (MyController) ctx.getBean("myController");
 
-        System.out.println(controller.hello());
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser());
+
     }
 }
---
+++ spring5di-demo/src/main/java/com/maciej/spring5didemo/config/PropertyConfig.java
@@ -0,0 +1,35 @@
+package com.maciej.spring5didemo.config;
+
+import com.maciej.spring5didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    String user;
+    @Value("${guru.password}")
+    String password;
+    @Value(("${guru.dburl}"))
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUser(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() { // it is reading the property file
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }
+}
