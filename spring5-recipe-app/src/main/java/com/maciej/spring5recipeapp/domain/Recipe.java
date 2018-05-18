ee3c27492344b10c30df54b91766c248c9977ead
Smolarz <mjsz@gft.com>
2018-05-18 13:44:49 +0200

    Added ManyToMany with Category

--- spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/domain/Recipe.java
@@ -34,6 +34,11 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -128,5 +133,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
