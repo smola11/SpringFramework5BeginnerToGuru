2c52dfc172ba2fc82e22b4b0934d41e3b66a802d
Smolarz <mjsz@gft.com>
2018-05-18 12:02:05 +0200

    Added Recipe and Notes OneToOne

---
+++ spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/domain/Notes.java
@@ -0,0 +1,43 @@
+package com.maciej.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // we allow the Recipe to own this;
+    // if we delete Notes object we don't want to delete Recipe object; Inverse we want;
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob // It will be created as CLOB in database to allow user more input;
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
