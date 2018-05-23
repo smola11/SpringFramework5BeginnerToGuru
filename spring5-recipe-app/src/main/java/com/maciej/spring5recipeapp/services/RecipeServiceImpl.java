3d880362175b8f4bdd3f62b61c670031f9e6b1be
Smolarz <mjsz@gft.com>
2018-05-23 12:20:34 +0200

    Added converters, CommandObjects and new save method with test

--- spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/services/RecipeServiceImpl.java
+++ spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/services/RecipeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.maciej.spring5recipeapp.services;
 
+import com.maciej.spring5recipeapp.commands.RecipeCommand;
+import com.maciej.spring5recipeapp.converters.RecipeCommandToRecipe;
+import com.maciej.spring5recipeapp.converters.RecipeToRecipeCommand;
 import com.maciej.spring5recipeapp.domain.Recipe;
 import com.maciej.spring5recipeapp.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Optional;
@@ -14,9 +18,13 @@ import java.util.Set;
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
@@ -34,5 +42,17 @@ public class RecipeServiceImpl implements RecipeService {
             throw new RuntimeException("Recipe Not Found");
         }
         return recipeOptional.get();
+    }
+
+
+    // We Make @Transactional because - We go through the converters outside of the spring context;
+    // Spring Data JPA repositories will implicitly start and end transaction;
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(command); // detached from Hibernate Context
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        log.debug("Saved RecipeId:" + savedRecipe);
+        return recipeToRecipeCommand.convert(savedRecipe);
     }
 }
--- spring5-recipe-app/src/test/java/com/maciej/spring5recipeapp/services/RecipeServiceImplTest.java
+++ spring5-recipe-app/src/test/java/com/maciej/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.maciej.spring5recipeapp.services;
 
+import com.maciej.spring5recipeapp.converters.RecipeCommandToRecipe;
+import com.maciej.spring5recipeapp.converters.RecipeToRecipeCommand;
 import com.maciej.spring5recipeapp.domain.Recipe;
 import com.maciej.spring5recipeapp.repositories.RecipeRepository;
 import org.junit.Before;
@@ -22,10 +24,16 @@ public class RecipeServiceImplTest {
     @Mock
     private RecipeRepository recipeRepository;
 
+    @Mock
+    RecipeToRecipeCommand recipeToRecipeCommand;
+
+    @Mock
+    RecipeCommandToRecipe recipeCommandToRecipe;
+
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this); // give us a Mock RecipeRepository
-        recipeService = new RecipeServiceImpl(recipeRepository);
+        recipeService = new RecipeServiceImpl(recipeRepository, recipeCommandToRecipe, recipeToRecipeCommand);
     }
 
     @Test
@@ -43,7 +51,7 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void getRecipeByIdTest(){
+    public void getRecipeByIdTest() {
         Recipe recipe = new Recipe();
         recipe.setId(1L);
         Optional<Recipe> recipeOptional = Optional.of(recipe);
