8c09ad424aeed89088f32b5b86ddcb5f4aae0a33
Smolarz <mjsz@gft.com>
2018-05-23 16:06:55 +0200

    Added updateRecipe feature

--- spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/controllers/RecipeController.java
+++ spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/controllers/RecipeController.java
@@ -15,7 +15,7 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
@@ -33,6 +33,12 @@ public class RecipeController {
     public String saveOrUpdate(@ModelAttribute RecipeCommand command) { // bind form post parameters to RecipeCommand object;
         RecipeCommand savedRecipe = recipeService.saveRecipeCommand(command);
 
-        return "redirect:/recipe/show/" + savedRecipe.getId();
+        return "redirect:/recipe/" + savedRecipe.getId() + "/show";
+    }
+
+    @RequestMapping("recipe/{id}/update")
+    public String updateRecipe(@PathVariable String id, Model model) {
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+        return "recipe/recipeform";
     }
 }
