1f0058c6deeae07738fb3e5c8ee1856faac1e6ac
Maciej  Smolarz <smola11@o2.pl>
2018-06-06 18:30:04 +0200

    Added @ExceptionHandler for NotFoundException

--- spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/controllers/RecipeController.java
+++ spring5-recipe-app/src/main/java/com/maciej/spring5recipeapp/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.maciej.spring5recipeapp.controllers;
 
 import com.maciej.spring5recipeapp.commands.RecipeCommand;
+import com.maciej.spring5recipeapp.exceptions.NotFoundException;
 import com.maciej.spring5recipeapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -46,5 +49,18 @@ public class RecipeController {
         log.debug("Deleting id: " + id);
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
