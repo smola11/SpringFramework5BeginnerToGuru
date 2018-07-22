c25c5982ff9c6a63b5c8dc79c98e769260f1665c
Maciej  Smolarz <smola11@o2.pl>
2018-07-22 10:47:51 +0200

    Edited unitOfMeasureSerive to be reactive - changes in controller

--- spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -29,7 +29,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     private final CategoryRepository categoryRepository;
     private final RecipeRepository recipeRepository;
     private final UnitOfMeasureRepository unitOfMeasureRepository;
-    
+
     public RecipeBootstrap(CategoryRepository categoryRepository,
                            RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
         this.categoryRepository = categoryRepository;
