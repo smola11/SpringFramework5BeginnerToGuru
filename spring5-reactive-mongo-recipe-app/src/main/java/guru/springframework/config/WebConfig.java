70a08bed422abee3abf255e5e00d0b6d62898501
Maciej  Smolarz <smola11@o2.pl>
2018-07-29 12:05:21 +0200

    Added / to the url

--- spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/config/WebConfig.java
+++ spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
 
     @Bean
     RouterFunction<?> routes(RecipeService recipeService) {
-        return RouterFunctions.route(GET("api/recipes"),
+        return RouterFunctions.route(GET("/api/recipes"),
                 serverRequest -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(recipeService.getRecipes(), Recipe.class));
