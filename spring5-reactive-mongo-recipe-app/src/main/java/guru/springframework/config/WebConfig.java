8429993784d6a4bc8baa5ffd3701954f19ec25c4
Maciej  Smolarz <smola11@o2.pl>
2018-07-29 12:01:59 +0200

    Added routing function for GET api/recipes request

---
+++ spring5-reactive-mongo-recipe-app/src/main/java/guru/springframework/config/WebConfig.java
@@ -0,0 +1,27 @@
+package guru.springframework.config;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class WebConfig {
+
+    // we route GET http method when we go to "api/recipes" - we provide ServerResponse
+    // with ok status and JSON
+
+    @Bean
+    RouterFunction<?> routes(RecipeService recipeService) {
+        return RouterFunctions.route(GET("api/recipes"),
+                serverRequest -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(recipeService.getRecipes(), Recipe.class));
+    }
+}
