06b761f49103c80c281037283f60a0909bc8bb99
Maciej  Smolarz <smola11@o2.pl>
2019-04-06 18:36:04 +0200

    Common CrudService interface

--- spring-pet-clinic/pet-clinic-data/src/main/java/com/maciej/springpetclinic/services/OwnerService.java
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/maciej/springpetclinic/services/OwnerService.java
@@ -2,16 +2,9 @@ package com.maciej.springpetclinic.services;
 
 import com.maciej.springpetclinic.model.Owner;
 
-import java.util.Set;
+public interface OwnerService extends CrudService<Owner, Long> {
 
-public interface OwnerService {
+    Owner findByLastName(String name);
 
-    Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- spring-pet-clinic/pet-clinic-data/src/main/java/com/maciej/springpetclinic/services/VetService.java
+++ spring-pet-clinic/pet-clinic-data/src/main/java/com/maciej/springpetclinic/services/VetService.java
@@ -2,13 +2,5 @@ package com.maciej.springpetclinic.services;
 
 import com.maciej.springpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
